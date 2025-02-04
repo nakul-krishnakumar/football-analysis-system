76699bbce76d2a973009720df984b84b6d1ec2aa
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-02-04 20:51:07 +0000

    Fine tuned the model using dataset from roboflow

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('models/best.pt')
+
+print(f"Model info:\n{model.info}")
 
 results = model.predict('input_videos/08fd33_4.mp4', save=True)
 print(results[0])
